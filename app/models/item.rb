c4210a1a02e399ce5309417d31a791a439d39c70
MargVander <margauxvanderbeken@gmail.com>
2019-03-04 13:09:28 +0100

    added some validation to the item table to make sure title and des is long enough and the prices cant be free

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :description, presence: true, length: { minimum: 15 }
+  validates :price, presence: true, :numericality => { greater_than: 0 }
+  validates :image_url, presence: true
 end
