651a784658a132327a9e0a085010c7352aad333a
MargVander <margauxvanderbeken@gmail.com>
2019-03-04 12:35:46 +0100

    modification of the number of items in the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'faker'
 
-10.times do
+20.times do
   item = Item.create(title: Faker::Creature::Cat.name, description: Faker::Lorem.paragraph, price: rand(1..100), image_url: "https://loremflickr.com/320/240?random=#{rand(1..10)}")
 end
