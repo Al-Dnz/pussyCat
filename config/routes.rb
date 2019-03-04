ae0ceac1b6a25c796d2a42785defe4208026c747
AlexDNZ <paleozene@gmail.com>
2019-03-04 12:34:23 +0100

    Ajout du controller item 	avec fonction Show , Index , Destroy 	Definition de la variable globale @items=Item.all
    Création des views index.html.erb et show.html.erb.
    Création d'une ressource :item dans les routes.
    Definition  de la page index en root principale.
    
    :w q
    :w
    :q

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :item , only: [:index , :shox, :destroy]
+
+  root 'item#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
