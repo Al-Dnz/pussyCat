ec120f9f1536c299657787ff017c85240e0ce1de
stephanesdr <stadlerstephane@gmail.com>
2019-03-04 16:11:24 +0100

    switch order require css and fix loop on index#item

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_03_04_111748) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,4 +35,5 @@ ActiveRecord::Schema.define(version: 2019_03_04_111748) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
