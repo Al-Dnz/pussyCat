00cb4d5319f204fb4b638439436301ac1fc6ddec
AlexDNZ <paleozene@gmail.com>
2019-03-04 13:00:55 +0100

    Modification des view index et show. Ajout d'un front minimal en bootstrapp card

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :item , only: [:index , :shox, :destroy]
+  resources :item , only: [:index , :show, :destroy]
 
   root 'item#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
