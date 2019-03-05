1d76c1487e7f2da0c87b661fbcc5390dd68526c5
AlexDNZ <paleozene@gmail.com>
2019-03-05 11:53:24 +0100

    updated master

--- app/controllers/cart_item_controller.rb
+++ app/controllers/cart_item_controller.rb
@@ -1,5 +1,4 @@
 class CartItemController < ApplicationController
-
   def new
     @cart_item = CartItem.new
   end
@@ -15,18 +14,4 @@ class CartItemController < ApplicationController
     @cart_item.destroy
     redirect_to cart_path(4)
   end
-
-
-  def show
-  	@cart_item = CartItem.new(item_id: params[:id], cart_id: current_user.cart.id)
-  	if @cart_item.save
-  		flash[:notice] = "add cart successfully"
-   		redirect_to :item
-  	else
-  		flash[:notice] = "add cart fail"
-  		redirect_to :item
-  	end
-  end
-
-
 end
