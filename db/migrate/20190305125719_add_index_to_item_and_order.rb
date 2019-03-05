fb05d49e0f1233354bf11fe0bcd1d31da005724c
stephanesdr <stadlerstephane@gmail.com>
2019-03-05 14:25:06 +0100

    migration model Order and ItemOrder ok test console relations check and ok

--- db/migrate/20190305125719_add_index_to_item_and_order.rb
+++ db/migrate/20190305125719_add_index_to_item_and_order.rb
@@ -1,6 +1,6 @@
-class AddIndexToItemAndOrders < ActiveRecord::Migration[5.2]
+class AddIndexToItemAndOrder < ActiveRecord::Migration[5.2]
   def change
     add_reference :item_orders, :item, foreign_key: true
     add_reference :item_orders, :order, foreign_key: true
-    end
   end
+end
