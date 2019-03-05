970425242b1d62b7135d8fa03b567efdb8e794a4
stephanesdr <stadlerstephane@gmail.com>
2019-03-05 13:50:39 +0100

    add model and migration element for order and item_order association

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,9 @@ class Item < ApplicationRecord
   validates :price, presence: true, :numericality => { greater_than: 0 }
   validates :image_url, presence: true
   has_many :cart_items
+  has_many :item_orders
   has_many :carts, through: :cart_items
+  has_many :orders, through: :item_orders
+
+
 end
---
+++ spec/factories/add_index_to_item_and_orders.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :add_index_to_item_and_order do
+    
+  end
+end
