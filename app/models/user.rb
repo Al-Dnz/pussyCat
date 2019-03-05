0fc8eeba081896cc4a4cea4cd733c2cb9b10999d
MargVander <margauxvanderbeken@gmail.com>
2019-03-05 10:54:01 +0100

    modification of the after create method so it looks cleaner

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :cart, dependent: :destroy
+  after_create :create_cart
+
+  def create_cart
+    cart = Cart.create(user_id: self.id)
+  end
 end
