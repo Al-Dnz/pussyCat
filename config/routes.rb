68d3526e7588b42fd0a93096aaa7db68e7335e20
LordGoblin <maxime78600@gmail.com>
2019-03-05 14:44:15 +0100

    fusionnnnnnnnnnnnnnn

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-  resources :cart_item
-  #get 'card/show'
-  resources :cart
-=======
 
->>>>>>> ce472e712aeb5b24ae8f607eaadeae5829ee4b41
   devise_for :users
   resources :item , only: [:index , :show, :destroy] do
     resources :cart_item , only: [:show, :destroy , :create]
   end
-  resources :cart , only: [:index , :show, :destroy ,:create]
+  resources :cart , only: [:index , :show, :destroy , :create, :update]
   #resources :cart_item , only: [:show, :destroy , :create]
 
 
