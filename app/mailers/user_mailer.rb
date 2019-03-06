020f9bf109b82543fe0cc14dfb86de6aa0e32a80
LordGoblin <maxime78600@gmail.com>
2019-03-06 01:30:20 +0100

    connect mailer at stripe/cart

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,15 @@
 class UserMailer < ApplicationMailer
 	default from: 'no-reply@pussyCat.fr'
- 
+  
+  def validate_buy_stripe(user)
+  	@user = user
+  	@url  = 'localhost:3000'
+
+  	mail(to: @user.email, subject: 'validation achat')
+  end
+
+
+
   def welcome_email(user)
     @user = user 
     @url  = 'localhost:3000' 
